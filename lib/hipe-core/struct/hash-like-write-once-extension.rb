6e757c520a52e619282a8f0eb73262bdc2edad7c
Mark Meves <mark.meves@gmail.com>
2009-12-19 20:30:27 -0500

    hash-like write once extension works, with tests.  About to try to test it on OrderedHash

--- lib/hipe-core/struct/hash-like-write-once-extension.rb
+++ lib/hipe-core/struct/hash-like-write-once-extension.rb
@@ -1,8 +1,8 @@
 module Hipe
   module HashLikeWriteOnceExtension
-    # With some data structures it is useful to assert that certain elements only 
+    # With some data structures it is useful to assert that certain elements only
     # get written zero or one time -- that is that that element is never clobbered.
-    # This mixin gives that ability to an OpenStruct. 
+    # This mixin gives that ability to an OpenStruct.
     #
     # @see Hipe::OpenStructLikeWriteOnceExtension, which this is based off of
     #
@@ -12,52 +12,83 @@ module Hipe
     #
     #     h = Hipe::HashLikeWriteOnceExtension[Hash.new]
     #     h.write_once! :title
-    #  
+    #
     #     h[:title] = "Behavior-centric client-side MVC with Jhaskell"
     #     h[:title] = "something else"    #=> throws a TypeError: "can't write to frozen index 'title'"
     #
-    # If you want to assert that any and all fields in the struct are only ever written once, 
-    #     
+    # If you want to assert that any and all fields in the struct are only ever written once,
+    #
     #     h.write_once!
     #
     # If you want to have more fine-grained control of the behavior on clobber,
-    # you can define a handler for it, with 
+    # you can define a handler for it, with
     #
     #     h.on_clobber{ |field_name| raise MyException.new(%{#{field_name} is already set.}) }
     #
     # At present, the above is only available for object-wide write-once struct, and not clobbers
-    # per field. 
+    # per field.
     #
 
     def self.[](table)
       table.extend self
       table
     end
-    
+
     def self.extended(obj)
       obj.hash_like_write_once_extension_init
-    end  
-    
+    end
+
     def hash_like_write_once_extension_init
       @hlwoe_orig_store = method(:store)
-      @hlwoe_orig_fetch = method(:fetch)
+      @on_clobber_any = nil      
+      @on_clobber = {}  
+      @frozen = {}
+      @write_once = {}
       meta = class << self; self end
-      
-      meta.send(:define_method,:store) do |key,value|
-        hlwoe_before_store(key,value)
+      meta.send(:define_method,:store) do |key,value|        
+        hlwoe_store(key,value)
+      end
+      meta.send(:alias_method, :[]=, :store)
+    end
+
+    def hlwoe_store(key,value)
+      if @frozen[key]
+        clobbering_time = (@on_clobber[key] || @on_clobber_default)
+        return self.instance_exec(key, value, &clobbering_time)
+      elsif (true==@write_once or @write_once[key])
+        @hlwoe_orig_store.call(key,value) # if this for some reason throws, we don't record the write
+        freeze_index! key
+      else
         @hlwoe_orig_store.call(key,value)
       end
-      
-      meta.send(:define_method,:fetch) do |key|
-        hlwoe_before_fetch(key)
-        @hlwoe_orig_fetch.call(key)        
+    end
+    
+    def write_once! *list, &block
+      if (list.size == 0)
+        @write_once = true
+        @on_clobber_default = block || on_clobber_default_default
+      else
+        @on_clobber ||= {}
+        @write_once = {} unless Hash === @write_once
+        list.each do |key|
+          if has_key? key
+            freeze_index! key
+          else
+            @write_once[key] = true
+          end
+          @on_clobber[key] = block || on_clobber_default_default          
+        end
       end
     end
-    def hlwoe_before_fetch(key)
-      puts "\ncalling fetch!"
+    
+    def on_clobber_default_default
+      @on_clobber_default_default ||= lambda{|key,value|
+        raise TypeError.new(%{can't overwrite frozen position #{key.inspect} of #{self.class}})
+      }
     end
-    def hlwoe_before_store(key,value)
-      puts "\ncalling store!"      
+    
+    def freeze_index!(key)
+      @frozen[key] = true
     end
   end
 end
--- spec/struct/spec_hash-like-write-once-extension.rb
+++ spec/struct/spec_hash-like-write-once-extension.rb
@@ -2,7 +2,10 @@
 require 'hipe-core'
 require 'hipe-core/struct/hash-like-write-once-extension'
 require 'ruby-debug'
+require File.expand_path(File.dirname(__FILE__)+'/../bacon-test-strap')
 
+class XyzzyException < Exception
+end
 
 describe Hipe::HashLikeWriteOnceExtension do
   it "on a plain old hash fetch and store still work (hl1)" do
@@ -11,4 +14,53 @@ describe Hipe::HashLikeWriteOnceExtension do
     h.store('alpha','beta')
     h.fetch('alpha').should.equal('beta')
   end
+
+  it "on a plain old hash fetch and store still work with []= and [] (hl2)" do
+    1.should.equal 1
+    @h = Hipe::HashLikeWriteOnceExtension[Hash.new]
+    @h['alpha'] = 'beta'
+    @h['alpha'].should.equal('beta')
+  end
+  
+  it "respects write_once!  before the fact without a block, and maintain the orig. value (hl3)" do
+    @h.write_once! :gamma
+    @h[:gamma] = 'gamma'
+    @h[:gamma].should.equal 'gamma'
+    e = lambda{@h[:gamma]='beta'}.should.raise(TypeError)
+    @h[:gamma].should.equal 'gamma'    
+    e.message.should.match %r{can't overwrite}
+  end
+  
+  it "respects write_once! after the fact without a block, and maintain the orig. value(hl4)" do
+    @h =  Hipe::HashLikeWriteOnceExtension[Hash.new]
+    @h[:gamma] = 'gammaz'
+    @h.write_once! :gamma
+    @h[:gamma].should.equal 'gammaz'
+    e = lambda{@h[:gamma]='beta'}.should.raise(TypeError)
+    @h[:gamma].should.equal 'gammaz'    
+    e.message.should.match %r{can't overwrite}
+  end
+  
+  it "should support custom clobber routines that throw, and maintain orig value (hl5)" do
+    @h =  Hipe::HashLikeWriteOnceExtension[Hash.new]    
+    @h.write_once!(:gamma){|key,value| raise XyzzyException.new(%{blah blah #{key}})}
+    @h[:gamma] = 'gammaz'
+    @h[:gamma].should.equal 'gammaz'
+    e = lambda{@h[:gamma]='beta'}.should.raise(XyzzyException)
+    e.message.should.match %r{blah blah gamma}    
+    @h[:gamma].should.equal 'gammaz'    
+  end  
+  
+  it "should support custom clobber routines that don't throw, and maintain orig value (hl6)" do
+    @h =  Hipe::HashLikeWriteOnceExtension[Hash.new]    
+    val = 'ok'
+    @h.write_once!(:gamma){|key,value|  val = 'not ok'}
+    @h[:gamma] = 'gammaz'
+    @h[:gamma].should.equal 'gammaz'
+    @h[:gamma]='beta'  # should do nothing    
+    @h[:gamma].should.equal 'gammaz'    
+    val.should.equal 'not ok'
+  end  
+  
+  
 end
